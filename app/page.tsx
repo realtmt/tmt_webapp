3a6805bad16b174dc2887030b452c77ea280824a
Beau Dekker <beau@patrickdekker.nl>
2023-04-12 00:23:56 +0200

    countdown

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
 import { Poppins } from "next/font/google";
+import CountdownTimer from "../components/CountdownTimer";
 
 const poppins = Poppins({ subsets: ["latin"], weight: "300" });
 
 export default function Home() {
-  return <h1 className={`${poppins.className} `}>Swiebeltje</h1>;
+  return (
+    <section
+      className={`${poppins.className} absolute left-0 top-0 w-full h-full flex justify-center items-center text-3xl font-light`}
+    >
+      <CountdownTimer />
+    </section>
+  );
 }
---
+++ components/CountdownTimer.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const CountdownTimer = () => {
+  const [countdown, setCountdown] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = new Date("2023-04-26").getTime() - now;
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      setCountdown({ days, hours, minutes, seconds });
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <h1>
+      {countdown.days} days, {countdown.hours} hours, {countdown.minutes}{" "}
+      minutes, {countdown.seconds} seconds
+    </h1>
+  );
+};
+
+export default CountdownTimer;
