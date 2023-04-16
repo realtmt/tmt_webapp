c286c6ecbe596ceae6b82b87aaa5bdd85cf0049c
Beau Dekker <beau@patrickdekker.nl>
2023-04-16 14:02:09 +0200

    live init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,40 @@
+import { Session } from "next-auth"
+import { headers } from "next/headers"
+import AuthContext from "./AuthContext"
+
 import "./globals.css"
+
+async function getSession(cookie: string): Promise<Session> {
+    const response = await fetch(
+        `${process.env.LOCAL_AUTH_URL}/api/auth/session`,
+        {
+            headers: {
+                cookie,
+            },
+        }
+    )
+
+    const session = await response.json()
+
+    return Object.keys(session).length > 0 ? session : null
+}
 
 export const metadata = {
     title: "Take me To",
     description: "Take me to store",
 }
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
+    const session = await getSession(headers().get("cookie") ?? "")
     return (
         <html lang="en">
-            <body className="bg-dark">{children}</body>
+            <body className="bg-dark">
+                <AuthContext session={session}>{children}</AuthContext>
+            </body>
         </html>
     )
 }
--- components/CountdownTimer.tsx
+++ components/CountdownTimer.tsx
@@ -2,11 +2,13 @@
 import { useEffect, useState } from "react"
 
 const CountdownTimer = () => {
-    const [countdown, setCountdown] = useState({
-        days: 0,
-        hours: 0,
-        minutes: 0,
-        seconds: 0,
+    const [countdown, setCountdown] = useState<{
+        [key: string]: number | null
+    }>({
+        days: null,
+        hours: null,
+        minutes: null,
+        seconds: null,
     })
 
     useEffect(() => {
@@ -34,32 +36,34 @@ const CountdownTimer = () => {
                 {Object.keys(countdown).map((u, i) => {
                     let unit = countdown[u as keyof typeof countdown]
                     let isFirst = Object.keys(countdown)[0] === u
+                    if (unit)
+                        return (
+                            <li
+                                key={i}
+                                className="flex items-end justify-center relative"
+                            >
+                                {!isFirst && (
+                                    <span
+                                        className={`text-[48px] xs:text-[78px] md:text-[144px] flex items-center font-bold h-[20px] xs:h-[60px] md:h-[100px] px-1 md:px-2 mb-3 xs:mb-4 md:mb-5`}
+                                    >
+                                        :
+                                    </span>
+                                )}
+                                <span className="flex flex-col justify-center items-center space-y-1">
+                                    <span className="capitalize text-[12px] xs:text-[24px] md:text-[36px] flex justify-center text-light/70">
+                                        {u}
+                                    </span>
 
-                    return (
-                        <li
-                            key={i}
-                            className="flex items-end justify-center relative"
-                        >
-                            {!isFirst && (
-                                <span
-                                    className={`text-[48px] xs:text-[78px] md:text-[144px] flex items-center font-bold h-[20px] xs:h-[60px] md:h-[100px] px-1 md:px-2 mb-3 xs:mb-4 md:mb-5`}
-                                >
-                                    :
+                                    <span
+                                        className={`text-[48px] xs:text-[78px] md:text-[144px] flex items-center font-bold h-[40px] xs:h-[80px] md:h-[120px]`}
+                                    >
+                                        {(unit as number) < 10
+                                            ? "0" + unit
+                                            : unit}
+                                    </span>
                                 </span>
-                            )}
-                            <span className="flex flex-col justify-center items-center space-y-1">
-                                <span className="capitalize text-[12px] xs:text-[24px] md:text-[36px] flex justify-center text-light/70">
-                                    {u}
-                                </span>
-
-                                <span
-                                    className={`text-[48px] xs:text-[78px] md:text-[144px] flex items-center font-bold h-[40px] xs:h-[80px] md:h-[120px]`}
-                                >
-                                    {(unit as number) < 10 ? "0" + unit : unit}
-                                </span>
-                            </span>
-                        </li>
-                    )
+                            </li>
+                        )
                 })}
             </ul>
         </h1>
